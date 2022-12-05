4c771d5fcac6348405fec5bbef91df76748ffede
Gabriel Carvalho <gabryelll.carvalho@gmail.com>
2022-12-05 08:08:44 -0300

    Adicionando link para carregar icons bootstrap

--- views/cursos/_form.php
+++ views/cursos/_form.php
@@ -11,7 +11,7 @@ use \kartik\date\DatePicker;
 /** @var app\models\Cursos $model */
 /** @var yii\widgets\ActiveForm $form */
 ?>
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <div class="cursos-form">
 
     <?php $form = ActiveForm::begin(
--- views/cursos/_search.php
+++ views/cursos/_search.php
@@ -9,6 +9,7 @@ use \kartik\date\DatePicker;
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <div class="cursos-search">
 
     <?php $form = ActiveForm::begin([
