298683275a01e6cbe3f2d01dee2019a676656755
Gabriel Carvalho <gabryelll.carvalho@gmail.com>
2022-12-05 10:42:27 -0300

    Corrigindo config db

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=desafiocast',
     'username' => 'root',
-    'password' => 'Kokakola!1',
+    'password' => 'SENHA AQUI',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
